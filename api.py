1f8b2f61953fbcec417671c27c28bf0683223e80
AdamJC20 <adamcummings20@googlemail.com>
2025-12-22 14:39:44 +0000

    put static scripts in static folder

--- api.py
+++ api.py
@@ -4,15 +4,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 
-@app.route('/habits', methods=['GET', 'POST'])
-def habits():
-    if request.method == 'GET':
-        return render_template('habits.html')
+# @app.route('/habits', methods=['GET', 'POST'])
+# def habits():
+#     if request.method == 'GET':
+#         return render_template('habits.html')
 
 
 if __name__ == "__main__":
