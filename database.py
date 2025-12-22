07f2fbd9c4a3fa13a4c57f0cb38034727924892d
Enchilada429 <omaryahya.contact@gmail.com>
2025-12-22 15:32:26 +0000

    Add hashing to passwords in database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@
 from os import environ as ENV, _Environ
 
 from re import match
+from bcrypt import gensalt, hashpw, checkpw
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
@@ -16,8 +17,13 @@ def get_mongodb_client(config: _Environ) -> MongoClient:
     )
 
 
+def get_hashed_password(password: str) -> str:
+    """Returns a hashed password from a given password."""
+    return hashpw(bytes(password, "utf-8"), gensalt())
+
+
 def create_account(mongodb_client: MongoClient, username: str, password: str, email: str) -> None:
-    """Create a HabitQuest account with username, password, and email.
+    """Create a HabitQuest account with username, password, and email. Hashes the password.
     Returns error if email has invalid format."""
 
     if not match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", email):
@@ -27,7 +33,7 @@ def create_account(mongodb_client: MongoClient, username: str, password: str, em
 
     account_collection.insert_one({
         "username": username,
-        "password": password,
+        "password": get_hashed_password(password),
         "email": email
     })
 
@@ -39,4 +45,4 @@ if __name__ == "__main__":
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 
-    create_account(client, "omar", "yahya", "example@gmail.com")
+    create_account(client, "omar", "yahya", "anotherexample@gmail.com")
