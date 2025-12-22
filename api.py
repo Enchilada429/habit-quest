59649d18b51423798a8fbdb0711a21f61e2d2b94
AdamJC20 <adamcummings20@googlemail.com>
2025-12-22 14:37:42 +0000

    added endpoints for static pages but will change this later

--- api.py
+++ api.py
@@ -1,7 +1,5 @@
-"""
-run with 'flask --app api run'
-"""
-from flask import Flask, render_template
+"""Flask REST API"""
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,3 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/habits', methods=['GET', 'POST'])
+def habits():
+    if request.method == 'GET':
+        return render_template('habits.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
